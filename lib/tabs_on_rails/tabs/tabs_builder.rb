fb395c58a788b07455ca8cec08e9219ce9909ccb
Ryan Lundie <ryan@b2blogic.net>
2011-12-21 12:04:59 -0500

    Don't span, just create links. Change class from current to active.

--- lib/tabs_on_rails/tabs/tabs_builder.rb
+++ lib/tabs_on_rails/tabs/tabs_builder.rb
@@ -46,9 +46,7 @@ module TabsOnRails
       #
       def tab_for(tab, name, options, item_options = {})
         item_options[:class] = item_options[:class].to_s.split(" ").push("active").join(" ") if current_tab?(tab)
-        content = @context.link_to_unless(current_tab?(tab), name, options) do
-          @context.content_tag(:span, name)
-        end
+        content = @context.link(current_tab?(tab), name, options)
         @context.content_tag(:li, content, item_options)
      end
 
